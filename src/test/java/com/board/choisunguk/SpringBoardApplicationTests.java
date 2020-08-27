7f0d954586b03a952f2e160d5a79ab571b1b004f
choisungwook <kgg1959@naver.com>
2020-08-27 21:18:18 +0900

    프로젝트 생성

---
+++ src/test/java/com/board/choisunguk/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.choisunguk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
