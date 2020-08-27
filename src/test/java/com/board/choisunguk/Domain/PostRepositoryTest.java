ec42e9ca512fa27b0779686c74c24aaa275eeb7c
choisungwook <kgg1959@naver.com>
2020-08-27 22:58:57 +0900

    Add 테스트: 게시글저장과 조회

---
+++ src/test/java/com/board/choisunguk/Domain/PostRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.board.choisunguk.Domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+    @Autowired PostRepository postRepository;
+
+    @After
+    public void Cleanup(){
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스그 게시글";
+        String content = "테스트 본문";
+
+
+        postRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .autor("test@test.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
